c7170e1141629350809d34e3aaa5542b310cc2e8
Anees <aneesnafeesimmu@gmail.com>
2024-10-07 22:13:16 +0530

    workouts-added

--- src/components/Workouts.jsx
+++ src/components/Workouts.jsx
@@ -2,16 +2,19 @@ import Card from 'react-bootstrap/Card';
 import batmanChest from "../assets/images/batman-chest.jpg"
 import batmanTriceps from "../assets/images/batman-triceps.jpg"
 import batmanBack from "../assets/images/batman-back.jpg"
-
+import batmanBiceps from "../assets/images/batman-biceps.jpg"
+import batmanShoulders from "../assets/images/batman-shoulders.jpg"
+import batmanLegs from "../assets/images/batman-legs.jpg"
+import batmanAbs from "../assets/images/batman-abs.jpg"
 
 const Workouts = () => {
     return (
         <div className="flex flex-col justify-center items-center">
             <h1 className='text-4xl font-semibold'>Workouts</h1>
-            <div className='flex flex-wrap w-11/12 m-2 space-x-28 items-center justify-center'>
-            <div className='bg-white rounded-lg'>
+            <div className='flex flex-wrap m-2 items-center justify-center'>
+            <div className='bg-white m-10 rounded-lg'>
             <Card className="relative m-2">
-                <Card.Img className="h-72 w-auto " variant="top" src={batmanChest} />
+                <Card.Img className="h-64 w-auto" variant="top" src={batmanChest} />
                 <Card.Body className="p-0">
                     <Card.Text className="absolute bottom-0 p-2 left-0 w-full text-2xl bg-black/50 bg-opacity-75 text-white text-left">
                         Chest Workouts
@@ -19,9 +22,9 @@ const Workouts = () => {
                 </Card.Body>
             </Card>
             </div>
-            <div className='bg-white m-3 rounded-lg'>
+            <div className='bg-white m-10 rounded-lg'>
             <Card className="relative m-2">
-                <Card.Img className="h-72 w-auto " variant="top" src={batmanTriceps} />
+                <Card.Img className="h-64 w-auto " variant="top" src={batmanTriceps} />
                 <Card.Body className="p-0">
                     <Card.Text className="absolute bottom-0 p-2 left-0 w-full text-2xl bg-black/50 bg-opacity-75 text-white text-left">
                         Triceps Workouts
@@ -29,9 +32,9 @@ const Workouts = () => {
                 </Card.Body>
             </Card>
             </div>
-            <div className='bg-white m-3 rounded-lg'>
+            <div className='bg-white m-10 rounded-lg'>
             <Card className="relative m-2">
-                <Card.Img className="h-72 w-auto " variant="top" src={batmanBack} />
+                <Card.Img className="h-64 w-auto " variant="top" src={batmanBack} />
                 <Card.Body className="p-0">
                     <Card.Text className="absolute bottom-0 p-2 left-0 w-full text-2xl bg-black/50 bg-opacity-75 text-white text-left">
                         Back Workouts
@@ -39,42 +42,42 @@ const Workouts = () => {
                 </Card.Body>
             </Card>
             </div>
-            <div className='bg-white m-3 rounded-lg'>
+            <div className='bg-white m-10 rounded-lg'>
             <Card className="relative m-2">
-                <Card.Img className="h-72 w-auto " variant="top" src={batmanChest} />
+                <Card.Img className="h-64 w-auto " variant="top" src={batmanBiceps} />
                 <Card.Body className="p-0">
                     <Card.Text className="absolute bottom-0 p-2 left-0 w-full text-2xl bg-black/50 bg-opacity-75 text-white text-left">
-                        Chest Workouts
+                        Biceps Workouts
                     </Card.Text>
                 </Card.Body>
             </Card>
             </div>
-            <div className='bg-white m-3 rounded-lg'>
+            <div className='bg-white m-10 rounded-lg'>
             <Card className="relative m-2">
-                <Card.Img className="h-72 w-auto " variant="top" src={batmanChest} />
+                <Card.Img className="h-64 w-auto " variant="top" src={batmanShoulders} />
                 <Card.Body className="p-0">
                     <Card.Text className="absolute bottom-0 p-2 left-0 w-full text-2xl bg-black/50 bg-opacity-75 text-white text-left">
-                        Chest Workouts
+                        Shoulder Workouts
                     </Card.Text>
                 </Card.Body>
             </Card>
             </div>
-            <div className='bg-white m-3 rounded-lg'>
+            <div className='bg-white m-10 rounded-lg'>
             <Card className="relative m-2">
-                <Card.Img className="h-72 w-auto " variant="top" src={batmanChest} />
+                <Card.Img className="h-64 w-auto " variant="top" src={batmanLegs} />
                 <Card.Body className="p-0">
                     <Card.Text className="absolute bottom-0 p-2 left-0 w-full text-2xl bg-black/50 bg-opacity-75 text-white text-left">
-                        Chest Workouts
+                        Legs Workouts
                     </Card.Text>
                 </Card.Body>
             </Card>
             </div>
-            <div className='bg-white m-3 rounded-lg'>
+            <div className='bg-white m-10 rounded-lg'>
             <Card className="relative m-2">
-                <Card.Img className="h-72 w-auto " variant="top" src={batmanChest} />
+                <Card.Img className="h-64 w-auto " variant="top" src={batmanAbs} />
                 <Card.Body className="p-0">
                     <Card.Text className="absolute bottom-0 p-2 left-0 w-full text-2xl bg-black/50 bg-opacity-75 text-white text-left">
-                        Chest Workouts
+                        Abs Workouts
                     </Card.Text>
                 </Card.Body>
             </Card>
